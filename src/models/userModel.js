172d7aa32e56b15ff9579d363a3a76e053d93ef9
neharepos <nehakeshrimail@gmail.com>
2025-07-03 15:00:35 +0530

    made the schema

---
+++ src/models/userModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import { unique } from "next/dist/build/utils";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true,
+    },
+    email:{
+        type: String,
+        required: [true, "Please provide a email"],
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: [true, "Provide provide a password"]
+    },
+    isVerified:{
+        type:  Boolean,
+        default: false,
+    },
+    isAdmin:{
+        type:Boolean,
+        default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+
+})
+
+const User = mongoose.models.users || mongoose.model("user", userSchema)
+
+export default User;
